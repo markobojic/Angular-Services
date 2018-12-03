3ce7e24777c305d60fab3ea0a6974a6d3190e9d0
Marko Bojic <markobo@ccbill.com>
2018-12-03 08:22:57 +0100

    Promises and asinc await

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { LoggerService } from './logger.service';
 import { Reader } from 'app/models/reader';
 import { Book } from 'app/models/book';
 import { allBooks, allReaders } from 'app/data';
+import { BookTrackerError } from './../../models/bookTrackerError';
 
 @Injectable()
 export class DataService {
   mostPopularBook: Book = allBooks[0];
 
-  constructor(private loggerService: LoggerService) { }
+  constructor(private loggerService: LoggerService,
+              private http: HttpClient) { }
 
-  getAllReaders(): Reader[] {
-    return allReaders;
+  getAuthorRecomendation(readerID: number): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (readerID > 0) {
+          resolve('Dr. Seuss');
+        } else {
+          reject('invalid reader ID');
+        }
+      }, 2000);
+    })
+  }
+
+  getAllReaders(): Observable<Reader[] | BookTrackerError> {
+    return this.http.get<Reader[]>('/api/readers')
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const dataError = new BookTrackerError();
+    dataError.errorNumber = 100;
+    dataError.message = error.statusText;
+    dataError.friendlyMessage = 'An error occurred retrieving data.';
+    return throwError(dataError);
   }
 
   getReaderById(id: number): Reader {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Book } from 'app/models/book';
 import { allBooks, allReaders } from 'app/data';
 import { Reader } from 'app/models/reader';
 import { LoggerService } from '../core/services/logger.service';
 import { DataService } from '../core/services/data.service';
+import { BookTrackerError } from './../models/bookTrackerError';
 
 @Component({
   selector: 'app-dashboard',
@@ -23,8 +25,21 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.loggerService.logger('Welcome to the book tracker');
     this.allBooks = this.dataService.getAllBooks();
-    this.allReaders = this.dataService.getAllReaders();
+    this.dataService.getAllReaders()
+      .subscribe(
+        (data: Reader[]) => this.allReaders = data,
+        (error: BookTrackerError) => console.log(error.friendlyMessage),
+        () => this.loggerService.logger('All done getting readers')
+      );
     this.mostPopularBook = this.dataService.mostPopularBook;
+
+    this.getAuthorRecommendation(1)
+        .catch(err => this.loggerService.error(err));
+  }
+
+  private async getAuthorRecommendation(readerID: number): Promise<void> {
+    const author: string = await this.dataService.getAuthorRecomendation(readerID);
+    this.loggerService.logger(author);
   }
 
   deleteBook(bookID: number): void {
